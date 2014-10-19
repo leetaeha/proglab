370f83b3263ddf641405233f3bd6c8b4e8692156
백인천 <peb317@gmail.com>
2014-10-19 17:12:52 +0900

    공용으로 쓰이는 common파일 구성 및 썸네일 생성부분 소스구성 1차완료

---
+++ lib/common.php
@@ -0,0 +1,6 @@
+<?php 
+$dir_path = "../image/";   // 이미지 폴더
+define("THUMBNAIL_DIRECTORY", "_thumbnail");  // 썸네일 파일 저장 폴더
+define("THUMBNAIL_SIZE", "128");  // 썸네일 이미지의 사이즈 조절
+define("THUMBNAIL_QUALITY", "80");  // 썸네일 이미지 품질
+?>
--- lib/thumbnail.php
+++ lib/thumbnail.php
@@ -1,47 +1,30 @@
 <?php
-
 /*
-
 ////////////////// 파일 업로드부분에 사용할 소스 //////////////////////
 
 $remakeThumb = $_GET["remakeThumb"];
-$filename = $file;
+
+$filename = $remakeThumb;
 $ext = substr(strrchr($filename,"."),1);	//확장자앞 .을 제거하기 위하여 substr()함수를 이용
 $ext = strtolower($ext);			//확장자를 소문자로 변환
 
-// 2.사진인 경우만 이미지 직접 보여줌
 if($ext == "jpg" || $ext == "gif" || $ext == "png")
 {
-	// 섬네일이 존재하는지 확인
-	//  1. 섬네일 폴더 유무부터 확인(존재하지 않으면 생성)
-	//  2. 섬네일 존재 유무 확인
-
-	//  1.썸네일디렉토리 확인
-	if(!file_exists($dir_path.THUMBNAIL_DIRECTORY))
-	{
-		mkdir($dir_path.THUMBNAIL_DIRECTORY);
-	}
-
-	//  2.썸네일파일확인 후 없으면 생성 or 썸네일재생성여부가 Y로 들어오면..
-	if(!file_exists($dir_path.THUMBNAIL_DIRECTORY."/".$file) || $remakeThumb == "Y")
+	// 원본파일 이미지 사이즈 확인
+	$img_info = getimagesize($dir_path.$file);
+	
+	//$img_info[0] => 68 //가로사이즈
+	//$img_info[1] => 100 //세로사이즈
+	//$img_info[bits] => 8 //이미지사이즈
+	
+	// 원본 이미지파일의 사이즈가 일정 이상일 경우 썸네일 생성 
+	if($img_info[0] > 128 || $img_info[1] > 128 || $img_info[bits] > 100)
 	{
 		// Create_Thumbnail(썸네일을 만들 파일의 경로, 썸네일파일명, 리샘플여부) : 리샘플여부를 true로 주면 단순 resize가 아닌 resampling을 시도
 		Create_Thumbnail($dir_path, $file, false);
 	}
-
-	// 원본파일 이미지 사이즈 확인
-	$imgInfoArray = getimagesize($dir_path.$file);
-
-	echo "<img src=\"".$dir_path.THUMBNAIL_DIRECTORY."/".$file."\" border=\"0\" style=\"cursor:pointer\" onClick=\"javascript:showPopUpImage('".$dir_path.$file."', ".$imgInfoArray[0].",".$imgInfoArray[1].")\"><br/>"; //이미지 보여주는 코드
-	echo "{$file}</a>";
-
-	// 이미지 리스트 "ㅵ"는 구분자(파일경로, 가로사이즈, 세로사이즈)
-	$imgSrcArray[$imgcnt] = $dir_path.$file."ㅵ".$imgInfoArray[0]."ㅵ".$imgInfoArray[1];
-
-	$imgcnt++;
 }
 */
-
 
 function Create_Thumbnail($pathNm, $fileNm, $resampleYn) {
 	$ext = substr(strrchr($fileNm,"."),1);	//확장자앞 .을 제거하기 위하여 substr()함수를 이용
@@ -51,10 +34,10 @@ function Create_Thumbnail($pathNm, $fileNm, $resampleYn) {
 	if($ext == "jpg" || $ext == "png" || $ext == "gif")
 	{
 		$file = $pathNm.$fileNm;
-		$imgInfoArray = getimagesize($file);
+		$img_infoArray = getimagesize($file);
 						
-		$imgWidth = $imgInfoArray[0];
-		$imgHeight = $imgInfoArray[1];
+		$imgWidth = $img_infoArray[0];
+		$imgHeight = $img_infoArray[1];
 		$widthHeightRatio = $imgHeight / $imgWidth;
 
 		// 가로가 긴 경우
@@ -83,10 +66,10 @@ function Create_Thumbnail($pathNm, $fileNm, $resampleYn) {
 		$dst = imagecreatetruecolor($imgWidth, $imgHeight);
 
 		if($resampleYn) {
-			imagecopyresampled($dst, $src, 0, 0, 0, 0, $imgWidth, $imgHeight, $imgInfoArray[0], $imgInfoArray[1]);
+			imagecopyresampled($dst, $src, 0, 0, 0, 0, $imgWidth, $imgHeight, $img_infoArray[0], $img_infoArray[1]);
 		}
 		else {
-			imagecopyresized($dst, $src, 0, 0, 0, 0, $imgWidth, $imgHeight, $imgInfoArray[0], $imgInfoArray[1]);
+			imagecopyresized($dst, $src, 0, 0, 0, 0, $imgWidth, $imgHeight, $img_infoArray[0], $img_infoArray[1]);
 		}
 
 		// jpg, png, gif에 따라 코드가 다름
